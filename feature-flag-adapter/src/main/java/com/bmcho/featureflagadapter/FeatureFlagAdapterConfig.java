316ce493a82c0aa1b1296530fa240b313ea3b6ee
bmcho <bmcho91@naver.com>
2025-06-11 17:34:22 +0900

    ehn: OpenFeatureflag Provider 개발 ~ing

--- feature-flag-adapter/src/main/java/com/bmcho/featureflagadapter/FeatureFlagAdapterConfig.java
+++ feature-flag-adapter/src/main/java/com/bmcho/featureflagadapter/FeatureFlagAdapterConfig.java
@@ -1,11 +1,45 @@
 package com.bmcho.featureflagadapter;
 
+import dev.openfeature.contrib.providers.flagd.Config;
+import dev.openfeature.contrib.providers.flagd.FlagdOptions;
+import dev.openfeature.contrib.providers.flagd.FlagdProvider;
+import dev.openfeature.sdk.Client;
+import dev.openfeature.sdk.OpenFeatureAPI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Objects;
 
 @Configuration
 @ComponentScan(basePackages = {"com.bmcho.featureflagadapter"})
 @RequiredArgsConstructor
 public class FeatureFlagAdapterConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public Client featureFlagAdapter() {
+        String flagdHost = environment.getProperty("FEATUREFLAG_ENGINE_HOST") == null ? "localhost" : environment.getProperty("FEATUREFLAG_ENGINE_HOST");
+        String flagdPort = environment.getProperty("FEATUREFLAG_ENGINE_PORT") == null ? "8013" : environment.getProperty("FEATUREFLAG_ENGINE_PORT");
+
+        FlagdProvider flagd = new FlagdProvider(
+            FlagdOptions.builder()
+                .resolverType(Config.Resolver.RPC)
+                .host(flagdHost)
+                .port(Integer.parseInt(Objects.requireNonNull(flagdPort)))
+                .tls(false)
+                .maxCacheSize(100)
+                .retryBackoffMs(100)
+                .deadline(3000)
+                .cacheType("lru")
+                .build()
+        );
+        OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        api.setProviderAndWait(flagd);
+        return api.getClient();
+    }
+
 }
--- feature-flag-adapter/src/main/java/com/bmcho/featureflagadapter/openfeatureflag/FlagAdapter.java
+++ feature-flag-adapter/src/main/java/com/bmcho/featureflagadapter/openfeatureflag/FlagAdapter.java
@@ -1,16 +1,26 @@
 package com.bmcho.featureflagadapter.openfeatureflag;
 
 import com.bmcho.featureflagusecase.port.GetStringValuePort;
+import dev.openfeature.sdk.Client;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FlagAdapter implements GetStringValuePort {
 
+    private final Client flagdClient;
+
     @Override
     public String getStringValue(GetStringValuePortRequest request) throws Exception {
-        return request.getKey();
+        String result = flagdClient.getStringValue(request.getKey(), "null");
+        if (result.equals("null")) {
+            log.warn("Flagd returned null for key: {}", request.getKey());
+            throw new Exception("Flagd returned null for key: {}");
+        }
+        return result;
     }
 
     @Override
